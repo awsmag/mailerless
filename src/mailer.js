0820797a2ec9ef42f599596f558f81bbd72fad08
Simranjeet Singh <simranjeet2589@gmail.com>
2020-03-22 11:03:02 +0530

    [Adding mailer.js with html]

--- src/mailer.js
+++ src/mailer.js
@@ -1,1 +1,26 @@
-// Add SES mailing code here
+const sender = process.env.SENDER;
+const replyto = process.env.REPLYTO;
+
+function sendHTMLEmail(mailer, email) {
+  return mailer.sendEmail({
+    "Destination": {
+      "ToAddresses": email.recipient.split(",")
+    },
+    "Message": {
+      "Body": {
+        "Html": {
+         "Charset": "UTF-8",
+         "Data": `${email.body}`
+        }
+       },
+       "Subject": {
+        "Charset": "UTF-8",
+        "Data": `${email.subject}`
+       }
+      },
+    "Source": email.sender || sender,
+    "ReplyToAddresses": email.replyto || replyto
+  }).promise();
+}
+
+module.exports = {sendHTMLEmail};
